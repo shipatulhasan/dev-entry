cdd065deec35d2fee7acd4cde5a2a4423dd2f635
shipatulhasan <shipatulhasan328@gmail.com>
2026-06-14 03:10:02 +0600

    added shadcn and dark light mode here

---
+++ app/components/shared/mode-toggle.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+export function ModeToggle() {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() =>
+        setTheme(
+          resolvedTheme === "dark"
+            ? "light"
+            : "dark"
+        )
+      }
+    >
+      {resolvedTheme === "dark" ? (
+        <Sun className="h-5 w-5" />
+      ) : (
+        <Moon className="h-5 w-5" />
+      )}
+    </Button>
+  );
+}
